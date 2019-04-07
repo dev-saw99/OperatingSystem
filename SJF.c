aa0d309147cc497ac9e9b088a2c4f254c8b5a6bd
SONUKUMARSAW <sonukumarsaw66@gmail.com>
2019-04-07 21:43:07 +0530

    Modification in the project, removed Struct process

--- SJF.c
+++ SJF.c
@@ -1,38 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//required libraries
-
-struct process
-{
-    int p_id;   //Process id
-    int b_time; //Burst Time
-    int a_time; //Arrival Time
-};
-//structure process stimulating one individual process.
-
-struct process create_processes()
-{
-    struct process processes;
-
-        scanf("%d ", &processes.p_id);
-    scanf("%d", &processes.a_time);
-    scanf("%d", &processes.b_time);
-    ;
-    return processes;
-}
-// method to create the process by the user
 
 int main()
 {
-    int N;
-    printf("\n\nEnter number of process : ");
-    scanf("%d", &N);
-    printf("\nEnter Process Detatils \n\n");
-    printf("Process ID     Arrival Time     Burst Time (seperated by space)\n");
-    struct process proc[N];
-    for (int i = 0; i < N; i++)
-    {
-        proc[i] = create_processes();
-    }
+	int i, total_burst = 0, totalt, x, n;
+	
+	int flg=1;
+	
+	float avg_wait_time, avg_turn_ar_time;
+	
+	printf("\nEnter no of processes: ");
+	scanf("%d", &n);
+	
+	int matrix[n][7];
+
+	printf("\nEnter Process_ID, Arrival_time and Burst_time (seperated by space): \n");
+	
+	for (i = 0; i < n; i++)
+	{
+		
+		scanf("%d %d %d", &matrix[i][0] ,&matrix[i][1] , &matrix[i][2]);
+		printf("---------\n");
+		if(matrix[i][1]==0 && flg>=0){
+			flg--;
+		} 
+		if(flg==-1){
+
+			printf("\nTwo processes can't have arrival time -> 0 \n");
+			exit(0);
+		}
+		total_burst += matrix[i][2];
+		matrix[i][6] = -1;
+	}
+
+	//check the process having least arrival time. start processing with that process.
+	
+	int min=999999,f=0,burst=0;
+	for(i=0;i<n;i++){
+		
+		if(min>matrix[i][1]){
+			printf("%d",matrix[i][1]);
+			min=matrix[i][1];f=i;
+			}
+	}
+	burst=matrix[f][2];matrix[f][6]=0;matrix[f][3]=matrix[f][1]+matrix[f][2];
+	
+	total_burst = total_burst + min;
+	totalt = burst;
+	while (totalt < total_burst)
+	{	min=999999;
+		for(i=0;i<n;i++){
+		if(min>matrix[i][1] && matrix[i][6]==-1) {min=matrix[i][1];f=i;}
+		}
+		burst=matrix[f][2];
+		matrix[f][6]=0;
+		matrix[f][3]=totalt+burst+2;
+		totalt += burst;
+									   
+	}
+	avg_turn_ar_time = 0.0;
+	avg_wait_time = 0.0;
+	printf("\n  Process  \tAT   \tBT   \tCT   \tTAT   \tWT   \t");
+	printf("\n----------------------------------------------------");
+	for (int j = 0; j < n; j++)
+	{
+		printf("\n|\t %d", matrix[j][0]);
+		printf("|\t %d", matrix[j][1]);
+		printf("|\t %d", matrix[j][2]);
+		printf("|\t %d", matrix[j][3]);
+		matrix[j][4] = matrix[j][3] - matrix[j][1];
+		printf("|\t %d", matrix[j][4]);
+		matrix[j][5] = matrix[j][4] - matrix[j][2];
+		printf("|\t %d |", matrix[j][5]);
+		avg_wait_time =  + matrix[j][5];
+		avg_turn_ar_time = avg_turn_ar_time + matrix[j][4];
+		printf("\n----------------------------------------------------");
+	}
+	avg_turn_ar_time = avg_turn_ar_time / n;
+	avg_wait_time = avg_wait_time / n;
+	printf("\n-----------------------");
+
+	printf("\n|Average TAT: %f|", avg_turn_ar_time);
+
+	printf("\n-----------------------");
+	printf("\n|Average WT: %f |", avg_wait_time);
+    
+	printf("\n-----------------------\n");
+	return 0;
 }
